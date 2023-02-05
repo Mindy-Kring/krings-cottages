4c959fccc358fcf109d3243f03878df26df64699
Terry Reynolds <terrancejreynolds@gmail.com>
2023-02-05 15:47:01 -0500

    add gatsy-plugin-netlify to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-scroll-reveal",
+    "gatsby-plugin-netlify",
       {
     resolve: `gatsby-source-filesystem`,
     options: {
